aa7c6ee563697df23d6f4244091a71015ea566ff
Melissa F <melissa.faulner@gmail.com>
2020-11-30 20:55:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   return_key = nil
   name_hash.each do |key, value|
     if min == nil || value <= min
-      return_key = "key"
+      return_key = key
       min = value
     end
   end
